1c49c68a118b579922ab221e3f123124e3d8eaa9
salvasal <salvatoresalernitano96@gmail.com>
2019-06-26 15:30:58 +0200

    Creazione page login

--- UniCook/src/app/app-routing.module.ts
+++ UniCook/src/app/app-routing.module.ts
@@ -2,8 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: './pages/login/login.module#LoginPageModule' }
 ];
 
 @NgModule({
