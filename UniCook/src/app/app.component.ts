f35776eeb66927d43766df7c559846d55c908453
salvasal <salvatoresalernitano96@gmail.com>
2019-06-26 14:53:15 +0200

    Installazione ngx-translate

--- UniCook/src/app/app.component.ts
+++ UniCook/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TranslateService } from '@ngx-translate/core';
+import { LinguaService } from './services/lingua.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +14,32 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private translate: TranslateService,
+    private linguaService: LinguaService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.initTranslate();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+  }
+
+  initTranslate() {
+    // Set the default language for translation strings, and the current language.
+    const linguaPreferita = this.linguaService.getLinguaPreferita();
+    this.translate.setDefaultLang(linguaPreferita);
+    this.linguaService.getLinguaAttuale().subscribe((lingua: string) => {
+      if (lingua != null) {
+        this.translate.use(lingua);
+      } else {
+        this.translate.use(linguaPreferita);
+        this.linguaService.updateLingua(linguaPreferita);
+      }
     });
   }
 }
--- UniCook/src/app/app.module.ts
+++ UniCook/src/app/app.module.ts
@@ -9,13 +9,30 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// The translate loader needs to know where to load i18n files
+// in Ionic's static asset pipeline.
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    }),
     IonicStorageModule.forRoot({
       name: 'unicook__mydb',
       driverOrder: ['indexeddb', 'sqlite', 'websql']
