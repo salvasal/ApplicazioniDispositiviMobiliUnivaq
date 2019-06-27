9ae5f968b697b469c14e482416bfdb9950091888
salvasal <salvatoresalernitano96@gmail.com>
2019-06-27 13:02:10 +0200

    Creazione FormLogin

--- UniCook/src/app/pages/login/login.page.ts
+++ UniCook/src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  private formLogin: FormGroup;
+  private loginTitle: string;
+  private loginSubTitle: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formbuilder: FormBuilder,
+              private translateService: TranslateService) {
   }
 
+  ngOnInit() {
+    this.formLogin = this.formbuilder.group({
+      username: ['username', Validators.compose([
+        Validators.required
+      ])],
+      password: ['password', Validators.compose([
+        Validators.required
+      ])],
+    });
+    this.initTranslate();
+  }
+
+  private initTranslate() {
+    this.translateService.get('LOGIN_ERROR_SUB_TITLE').subscribe((data) => {
+      this.loginSubTitle = data;
+    });
+    this.translateService.get('LOGIN_ERROR_TITLE').subscribe((data) => {
+      this.loginTitle = data;
+    });
+  }
 }
