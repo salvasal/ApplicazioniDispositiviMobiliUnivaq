cad555ef28e58d1462246291a9a1d4b9ef2b4240
salvasal <salvatoresalernitano96@gmail.com>
2019-06-28 12:26:04 +0200

    Creazione Completa Page Login

--- UniCook/src/app/pages/login/login.page.ts
+++ UniCook/src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {TranslateService} from '@ngx-translate/core';
+import {AlertController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,26 +12,58 @@ export class LoginPage implements OnInit {
   private formLogin: FormGroup;
   private loginTitle: string;
   private loginSubTitle: string;
+  private loginSubTitleTextError: string;
 
   constructor(private formbuilder: FormBuilder,
-              private translateService: TranslateService) {
+              private translateService: TranslateService,
+              private alertController: AlertController,
+              private navController: NavController) {
   }
 
   ngOnInit() {
     this.formLogin = this.formbuilder.group({
-      username: ['username', Validators.compose([
+      username: ['ludo196', Validators.compose([
         Validators.required
       ])],
-      password: ['password', Validators.compose([
+      password: ['random01', Validators.compose([
         Validators.required
       ])],
     });
     this.initTranslate();
   }
 
+  onLogin() {
+    this.navController.navigateRoot('login');
+  }
+  onRecovery() {
+    this.navController.navigateRoot('login');
+  }
+  onRegister() {
+    this.navController.navigateRoot('login');
+  }
+  async showLoginTextError() {
+    const alert = await this.alertController.create({
+      message: this.loginSubTitleTextError,
+      buttons: ['OK']
+    });
+  }
+
+  async showLoginError() {
+    const alert = await this.alertController.create({
+      header: this.loginTitle,
+      message: this.loginSubTitle,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
   private initTranslate() {
     this.translateService.get('LOGIN_ERROR_SUB_TITLE').subscribe((data) => {
       this.loginSubTitle = data;
+    });
+    this.translateService.get('LOGIN_ERROR_SUB_TEXT_TITLE').subscribe((data) => {
+      this.loginSubTitleTextError = data;
     });
     this.translateService.get('LOGIN_ERROR_TITLE').subscribe((data) => {
       this.loginTitle = data;
