cac43a7c8ad41db9c98b96f0c7ca44177c6eca0d
lors10 <lors.93.10@gmail.com>
2019-07-03 12:28:23 +0200

    creazione utente.service.ts
    creazione utente.service.ts

--- UniCook/src/app/constants.ts
+++ UniCook/src/app/constants.ts
@@ -1,3 +1,17 @@
 
 
 export const LINGUA = 'lingua';
+
+export const X_AUTH = 'X-Auth';
+
+export const AUTH_TOKEN = 'auth-token';
+
+export const UTENTE_STORAGE = 'utente';
+
+export const USE_PROXY = true;
+
+export const URL_BASE = USE_PROXY ? 'api' : 'http://localhost:8080/unicook/api';
+
+export const URL = {
+    LOGIN: URL_BASE + '/login'
+};
