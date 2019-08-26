463828fafd2f5219ac9da833dd82eca44424d1d8
lors10 <lors.93.10@gmail.com>
2019-08-26 11:15:17 +0200

    modifica pages 'lista', 'valutazione'
    Implementazione funzione per bottone 'commenta' della pagina 'valutazione' con "alert".
    Implementazione funzione per bottone 'filtri' della pagina 'lista' con "action-sheet".

--- UniCook/src/app/pages/lista/lista.page.ts
+++ UniCook/src/app/pages/lista/lista.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {ActionSheetController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-lista',
@@ -8,12 +8,46 @@ import {NavController} from '@ionic/angular';
 })
 export class ListaPage implements OnInit {
 
-  constructor(private navController: NavController) { }
+  constructor(private navController: NavController,
+              private actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
   onDetails() {
     this.navController.navigateRoot('dettaglioricetta');
+  }
+  async funzioneAction() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Cerca ricette per:',
+      buttons: [
+          {
+        text: 'Difficolta',
+        role: 'search for difficult',
+        handler: () => {
+          console.log('Cerca per difficolta');
+        }
+      }, {
+          text: 'Nome',
+          role: 'search for name',
+          handler: () => {
+            console.log('Cerca per nome');
+          }
+        }, {
+          text: 'Tempo di realizzazione',
+          role: 'search for time',
+          handler: () => {
+            console.log('Cerca per tempo');
+          }
+        }, {
+          text: 'Cancella',
+          role: 'destructive',
+          handler: () => {
+            console.log('cancella');
+          }
+        }
+        ]
+    });
+    await actionSheet.present();
   }
 
 }
--- UniCook/src/app/pages/valutazione/valutazione.page.ts
+++ UniCook/src/app/pages/valutazione/valutazione.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NavController} from '@ionic/angular';
+import {AlertController, NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-valutazione',
@@ -8,12 +8,30 @@ import {NavController} from '@ionic/angular';
 })
 export class ValutazionePage implements OnInit {
 
-  constructor(private navController: NavController) { }
+  constructor(private navController: NavController,
+              private alertController: AlertController) { }
 
   ngOnInit() {
   }
   backtolistaricette() {
     this.navController.navigateRoot('listaricette/lista');
+  }
+  async funzioneAlert() {
+    const alert = await this.alertController.create({
+      header: 'Attenzione',
+      message: 'Per commentare devi aver effettuato il Login o essere registrato',
+      buttons: [
+        {
+          text: 'Torna alla login',
+          handler: () => {
+            this.navController.navigateRoot('login');
+          }
+        }, {
+        text: 'Indietro',
+        }
+      ]
+    });
+    await alert.present();
   }
 
 }
