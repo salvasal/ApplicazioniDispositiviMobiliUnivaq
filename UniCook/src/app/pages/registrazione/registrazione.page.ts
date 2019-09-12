11aab9a4f64ff3573b9ab55a3b2bef6d2994fc4f
lors10 <lors.93.10@gmail.com>
2019-09-12 17:13:17 +0200

    modifiche page "registrazione"

--- UniCook/src/app/pages/registrazione/registrazione.page.ts
+++ UniCook/src/app/pages/registrazione/registrazione.page.ts
@@ -11,13 +11,32 @@ import {TranslateService} from '@ngx-translate/core';
 export class RegistrazionePage implements OnInit {
   private registerForm: FormGroup;
   private registerSubTitle: string;
+  // private utente: Utente;
 
   constructor(private navController: NavController,
               private formBuilder: FormBuilder,
               private translateService: TranslateService) {
+              // private modalController: ModalController
+              // private navParams: NavParams
   }
 
   ngOnInit() {
+    /* funzione finale
+    *
+    this.utente = this.navParams.data.appParam;
+    this.registerForm = this.formBuilder.group({
+      nome: [this.utente.nome, Validators.compose([Validators.required])],
+      cognome: [this.utente.cognome, Validators.compose([Validators.required])],
+      datanascita: [this.utente.datanascita, Validators.compose([Validators.required])],
+      sesso: [this.utente.sesso, Validators.compose([Validators.required])],
+      usernamer: [this.utente.username, Validators.compose([Validators.required])],
+      passwordGroup: this.formBuilder.group({
+        passwordr: [this.utente.password, Validators.compose([Validators.required])],
+        pconfirm: [this.utente.password, Validators.compose([Validators.required])],
+      })
+    });
+    this.initTranslate();
+    */
     this.registerForm = this.formBuilder.group({
       nome: ['', Validators.compose([
         Validators.required
@@ -51,6 +70,16 @@ export class RegistrazionePage implements OnInit {
   }
   onRegister() {
     this.navController.navigateRoot('login');
+    /* come la collego ?
+    *
+    this.utente.nome = this.registerForm.value.nome;
+    this.utente.cognome = this.registerForm.value.cognome;
+    this.utente.datanascita = this.registerForm.value.datanascita;
+    this.utente.sesso = this.registerForm.value.sesso;
+    this.utente.username = this.registerForm.value.usernamer;
+    this.utente.password = this.registerForm.value.passwordr && this.registerForm.value.pconfirm;
+    * chiave di recupero ?
+    */
   }
 
   private initTranslate() {
