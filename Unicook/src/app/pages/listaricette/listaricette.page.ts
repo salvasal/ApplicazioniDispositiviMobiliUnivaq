5b6724ef4a72243919d313da10bbd8df2cc25fbd
lors10 <lors.93.10@gmail.com>
2020-06-24 19:55:32 +0200

    implementazione listaricette

--- Unicook/src/app/pages/listaricette/listaricette.page.ts
+++ Unicook/src/app/pages/listaricette/listaricette.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Ricetta} from '../../models/ricetta.models';
+import {RicettaService} from '../../services/ricetta.service';
 
 @Component({
   selector: 'app-listaricette',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaricettePage implements OnInit {
 
-  constructor() { }
+  private ricette: Ricetta[] = [];
+  private ricetteFiltered: Ricetta[] = [];
+
+  constructor(private ricettaService: RicettaService) { }
 
   ngOnInit() {
+    this.ricettaService.getAll().subscribe(result => {
+      this.ricette = result;
+      this.ricetteFiltered = result;
+      // tslint:disable-next-line:no-shadowed-variable
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getListFiltered(event) {
+    const val = event.target.value; // recupera il valore immesso dall'utente
+    if (val && val.trim() !== '') {
+      this.ricetteFiltered = this.ricette.filter( (element) => {
+        return element.nomericetta.toLowerCase().indexOf(val.toLowerCase()) > -1;
+      });
+      console.log(this.ricetteFiltered);
+    } else { this.ricetteFiltered = this.ricette; }
   }
 
 }
--- Unicook/src/app/services/ricetta.service.ts
+++ Unicook/src/app/services/ricetta.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Ricetta} from '../models/ricetta.models';
 import {URL_API} from '../constants';
 
@@ -15,7 +15,126 @@ export class RicettaService {
 
   // Metodo che permette di recuperare tutte le ricette
   getAll(): Observable<Ricetta[]> {
-    return this.http.get<Ricetta[]>(URL_API.allRicette);
+    // return this.http.get<Ricetta[]>(URL_API.allRicette);
+
+    this.listaRicetta = [{
+      IDricetta: 0,
+      nomericetta: 'Antipasto misto',
+      descrizione: '',
+      preparazione: '',
+      tempocottura: 10,
+      difficolta: '',
+      data: '24/06/2020',
+      ora: '18:30',
+      categoria: { IDcategoria: 0, nome: 'Antipasti' },
+      ingredienti: [{ IDingrediente: 0, nome: 'Formaggio'},
+        { IDingrediente: 1, nome: 'Salumi'}],
+      immagini: [{ filepath: '', webviewPath: 'assets/images/antipasto.jpg', base64: ''}],
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      }}, {
+      IDricetta: 1,
+      nomericetta: 'Carbonara',
+      descrizione: '',
+      preparazione: '',
+      tempocottura: 30,
+      difficolta: '',
+      data: '24/06/2020',
+      ora: '18:30',
+      categoria: { IDcategoria: 1, nome: 'Primi Piatti' },
+      ingredienti: [{ IDingrediente: 2, nome: 'Uova'},
+        { IDingrediente: 3, nome: 'Pasta'},
+        { IDingrediente: 4, nome: 'Guanciale'},
+        { IDingrediente: 5, nome: 'Pecorino'}],
+      immagini: [{ filepath: '', webviewPath: 'assets/images/primo.jpg', base64: ''}],
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      }}, {
+      IDricetta: 2,
+      nomericetta: 'Bistecca ai ferri',
+      descrizione: '',
+      preparazione: '',
+      tempocottura: 20,
+      difficolta: '',
+      data: '24/06/2020',
+      ora: '18:30',
+      categoria: { IDcategoria: 2, nome: 'Secondi Piatti' },
+      ingredienti: [{ IDingrediente: 6, nome: 'Carne'},
+        { IDingrediente: 7, nome: 'Sale'},
+        { IDingrediente: 8, nome: 'Pepe'}],
+      immagini: [{ filepath: '', webviewPath: 'assets/images/secondo.jpeg', base64: ''}],
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      }}, {
+      IDricetta: 3,
+      nomericetta: 'Bastoncini di zucchine',
+      descrizione: '',
+      preparazione: '',
+      tempocottura: 15,
+      difficolta: '',
+      data: '24/06/2020',
+      ora: '18:30',
+      categoria: { IDcategoria: 3, nome: 'Contorni' },
+      ingredienti: [{ IDingrediente: 9, nome: 'Zucchine'}],
+      immagini: [{ filepath: '', webviewPath: 'assets/images/contorno.jpg', base64: ''}],
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      }}, {
+      IDricetta: 4,
+      nomericetta: 'Tiramisu',
+      descrizione: '',
+      preparazione: '',
+      tempocottura: 60,
+      difficolta: '',
+      data: '24/06/2020',
+      ora: '18:30',
+      categoria: { IDcategoria: 4, nome: 'Dolci' },
+      ingredienti: [{ IDingrediente: 2, nome: 'Uova'},
+        { IDingrediente: 10, nome: 'Mascarpone'},
+        { IDingrediente: 11, nome: 'Cacao'},
+        { IDingrediente: 12, nome: 'Caffe'},
+        {IDingrediente: 13, nome: 'Savoiardi'}],
+      immagini: [{ filepath: '', webviewPath: 'assets/images/dolce.jpg', base64: ''}],
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      }}
+    ];
+    return of(this.listaRicetta);
   }
 
   // Metodo che permette di recuperare una ricetta passando come parametro il codice identificativo
