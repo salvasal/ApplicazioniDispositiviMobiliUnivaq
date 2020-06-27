c38ebd2b9e7d313a5ac2d71584c46053930e643c
Salvatore <s.salernitano@gp-start.it>
2020-06-27 19:31:10 +0200

    Implementazione Logica e Page Valutazione

--- Unicook/src/app/pages/valutazione/valutazione.page.ts
+++ Unicook/src/app/pages/valutazione/valutazione.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {Ricetta} from '../../models/ricetta.models';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {RicettaService} from '../../services/ricetta.service';
+import {ValutazioneService} from '../../services/valutazione.service';
+import {Valutazione} from '../../models/valutazione.models';
+import {Utente} from '../../models/utente.models';
+import {UtenteService} from '../../services/utente.service';
 
 @Component({
   selector: 'app-valutazione',
@@ -11,17 +15,114 @@ import {RicettaService} from '../../services/ricetta.service';
 export class ValutazionePage implements OnInit {
   id = null;
   ricetta: Ricetta = new Ricetta();
+  likes: Valutazione[] = [];
+  unlikes: Valutazione[] = [];
+  isEvaluateLike = false;
+  isEvaluateUnlike = false;
+  utente: Utente = new Utente();
+  valutazione: Valutazione = new Valutazione();
+  today: Date;
+  evaluationCurrent: number;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private ricettaService: RicettaService) { }
+              private ricettaService: RicettaService,
+              private router: Router,
+              private valutazioneService: ValutazioneService,
+              private utenteService: UtenteService) { }
 
   ngOnInit() {
     this.id = this.activatedRoute.snapshot.paramMap.get('id');
     this.ricettaService.get(this.id).subscribe(result => {
       this.ricetta = result;
+      this.getNumberLikesUnlikes();
     }, error => {
       console.error(error);
     });
+
+    this.utente = this.utenteService.getUtente().value;
+
+    this.valutazioneService.getAll().subscribe(result => {
+      for (const element of result) {
+        if (element.ricetta.IDricetta === this.ricetta.IDricetta && element.utente.IDutente === this.utente.IDutente) {
+          if (element.like === true) {
+            this.isEvaluateLike = true;
+            this.evaluationCurrent = element.IDvalutazione;
+          } else {
+            this.isEvaluateUnlike = true;
+            this.evaluationCurrent = element.IDvalutazione;
+          }
+        }
+      }
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  getNumberLikesUnlikes() {
+    this.valutazioneService.getAll().subscribe(result => {
+      for (const element of result) {
+        if (element.ricetta.IDricetta === this.ricetta.IDricetta && element.like === true ) {
+          this.likes.push(element);
+        }
+        if (element.ricetta.IDricetta === this.ricetta.IDricetta && element.unlike === true ) {
+          this.unlikes.push(element);
+        }
+      }
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  insertLike() {
+    this.valutazione.like = true;
+    this.valutazione.unlike = false;
+    this.valutazione.utente = this.utente;
+    this.valutazione.ricetta = this.ricetta;
+    this.today = new Date();
+    this.valutazione.data = this.today.getDate() + '-' + (this.today.getMonth() + 1) + '-' + this.today.getFullYear();
+    this.valutazioneService.insert(this.valutazione).subscribe(result => {
+      this.getNumberLikesUnlikes();
+      this.deleteUnlike();
+      this.isEvaluateLike = true;
+      this.valutazione = null;
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  insertUnlike() {
+    this.valutazione.like = false;
+    this.valutazione.unlike = true;
+    this.valutazione.utente = this.utente;
+    this.valutazione.ricetta = this.ricetta;
+    this.today = new Date();
+    this.valutazione.data = this.today.getDate() + '-' + (this.today.getMonth() + 1) + '-' + this.today.getFullYear();
+    this.valutazioneService.insert(this.valutazione).subscribe(result => {
+      this.getNumberLikesUnlikes();
+      this.deleteLike();
+      this.isEvaluateUnlike = true;
+      this.valutazione = null;
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  deleteLike() {
+    this.valutazioneService.delete(this.evaluationCurrent).subscribe(result => {
+      this.getNumberLikesUnlikes();
+      this.isEvaluateLike = false;
+    });
+  }
+
+  deleteUnlike() {
+    this.valutazioneService.delete(this.evaluationCurrent).subscribe(result => {
+      this.getNumberLikesUnlikes();
+      this.isEvaluateUnlike = false;
+    });
+  }
+
+  goToDettaglioRicetta() {
+    this.router.navigate(['/dettaglioricetta', this.id]);
   }
 
 }
--- Unicook/src/app/services/valutazione.service.ts
+++ Unicook/src/app/services/valutazione.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Valutazione} from '../models/valutazione.models';
 import {URL_API} from '../constants';
 
@@ -9,10 +9,88 @@ import {URL_API} from '../constants';
 })
 export class ValutazioneService {
 
+  private valutazioni: Valutazione[] = [
+    { IDvalutazione: 0,
+      data: '27/06/2020',
+      like: true,
+      unlike: false,
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      },
+      ricetta: {
+        IDricetta: 0,
+        nomericetta: 'Antipasto misto',
+        descrizione: 'Questa è una ricetta fake per verificare se il contenuto del campo descrizione sia totalmente leggibile',
+        preparazione: 'Questa è una ricetta fake per verificare se il contenuto del campo preparazione sia totalmente leggibile',
+        tempocottura: 10,
+        difficolta: 'MEDIO',
+        data: '24/06/2020',
+        ora: '18:30',
+        categoria: { IDcategoria: 0, nome: 'Antipasti' },
+        ingredienti: [{ IDingrediente: 0, nome: 'Formaggio'},
+          { IDingrediente: 1, nome: 'Salumi'}],
+        immagini: [{ filepath: '', webviewPath: 'assets/images/antipasto.jpg', base64: ''}],
+        utente: {
+          IDutente: 0,
+          username: 'lorsalvi',
+          password: 'ciao',
+          chiaverecupero: 'chiave1',
+          nome: '',
+          cognome: '',
+          sesso: '',
+          datanascita: ''
+        }}}, {
+      IDvalutazione: 1,
+      data: '25/06/2020',
+      like: false,
+      unlike: true,
+      utente: {
+        IDutente: 0,
+        username: 'lorsalvi',
+        password: 'ciao',
+        chiaverecupero: 'chiave1',
+        nome: '',
+        cognome: '',
+        sesso: '',
+        datanascita: ''
+      },
+      ricetta: {
+        IDricetta: 0,
+        nomericetta: 'Antipasto misto',
+        descrizione: 'Questa è una ricetta fake per verificare se il contenuto del campo descrizione sia totalmente leggibile',
+        preparazione: 'Questa è una ricetta fake per verificare se il contenuto del campo preparazione sia totalmente leggibile',
+        tempocottura: 10,
+        difficolta: 'MEDIO',
+        data: '24/06/2020',
+        ora: '18:30',
+        categoria: { IDcategoria: 0, nome: 'Antipasti' },
+        ingredienti: [{ IDingrediente: 0, nome: 'Formaggio'},
+          { IDingrediente: 1, nome: 'Salumi'}],
+        immagini: [{ filepath: '', webviewPath: 'assets/images/antipasto.jpg', base64: ''}],
+        utente: {
+          IDutente: 0,
+          username: 'lorsalvi',
+          password: 'ciao',
+          chiaverecupero: 'chiave1',
+          nome: '',
+          cognome: '',
+          sesso: '',
+          datanascita: ''
+        }}}
+  ];
+
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Valutazione[]> {
-    return this.http.get<Valutazione[]>(URL_API.allValutazioni);
+    // return this.http.get<Valutazione[]>(URL_API.allValutazioni);
+    return of(this.valutazioni);
   }
 
   get(id: number): Observable<Valutazione> {
