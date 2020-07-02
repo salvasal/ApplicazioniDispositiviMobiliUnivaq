1858882074bcc6d76bd1af96387b68e27fb83e62
Salvatore <s.salernitano@gp-start.it>
2020-07-02 10:43:11 +0200

    Implementazione Page Preferiti

--- Unicook/src/app/pages/preferiti/preferiti.page.ts
+++ Unicook/src/app/pages/preferiti/preferiti.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Preferito} from '../../models/preferito.models';
+import {Utente} from '../../models/utente.models';
+import {PreferitoService} from '../../services/preferito.service';
+import {UtenteService} from '../../services/utente.service';
 
 @Component({
   selector: 'app-preferiti',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreferitiPage implements OnInit {
 
-  constructor() { }
+  preferiti: Preferito[] = [];
+  private utente: Utente = new Utente();
+
+  constructor(private preferitoService: PreferitoService,
+              private utenteService: UtenteService) { }
 
   ngOnInit() {
+    this.utente = this.utenteService.getUtente().value;
+
+    this.preferitoService.getAll().subscribe(result => {
+      for (const element of result) {
+        if ( element.utente === this.utente ) {
+          this.preferiti.push(element);
+        }
+      }
+    }, error => {
+      console.error(error);
+    });
   }
 
 }
