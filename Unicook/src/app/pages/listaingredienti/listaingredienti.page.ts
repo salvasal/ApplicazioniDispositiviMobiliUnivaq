f85a87deeddbd3d1f78f3eda4a55f6f9e549fe38
Ludo196 <ludovico.difederico@outlook.it>
2020-07-03 17:43:56 +0200

    Implementazione page listaingredienti

--- Unicook/src/app/pages/listaingredienti/listaingredienti.page.ts
+++ Unicook/src/app/pages/listaingredienti/listaingredienti.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingrediente} from '../../models/ingrediente.models';
+import {IngredienteService} from '../../services/ingrediente.service';
 
 @Component({
   selector: 'app-listaingredienti',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listaingredienti.page.scss'],
 })
 export class ListaingredientiPage implements OnInit {
+  ingredienti: Ingrediente[] = [];
+  selectedArray: Ingrediente[] = [];
 
-  constructor() { }
+  constructor(private ingredienteService: IngredienteService) { }
 
   ngOnInit() {
+    this.ingredienteService.getAll().subscribe(result => {
+      this.ingredienti = result;
+      this.order();
+    }, error => {
+      console.error(error);
+    });
+  }
+
+  order(){
+    this.ingredienti.sort((a, b): number => {
+      if (a.nome < b.nome) { return -1; }
+      if (a.nome > b.nome) { return 1; }
+      return 0;
+    });
+  }
+
+  selectMember(data, event){
+    if (!event.target.checked) {
+      this.selectedArray.push(data);
+    } else {
+      const newArray = this.selectedArray.filter(el => {
+        return el.IDingrediente !== data.IDingrediente;
+      });
+      this.selectedArray = newArray;
+    }
+    console.log(this.selectedArray);
   }
 
 }
--- Unicook/src/app/services/ingrediente.service.ts
+++ Unicook/src/app/services/ingrediente.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {Ingrediente} from '../models/ingrediente.models';
 import {URL_API} from '../constants';
 
@@ -8,12 +8,27 @@ import {URL_API} from '../constants';
   providedIn: 'root'
 })
 export class IngredienteService {
+  ingredienti: Ingrediente[] = [{IDingrediente: 0, nome: 'Formaggio'},
+    {IDingrediente: 1, nome: 'Salumi'},
+    {IDingrediente: 2, nome: 'Uova'},
+    {IDingrediente: 3, nome: 'Pasta'},
+    {IDingrediente: 4, nome: 'Guanciale'},
+    {IDingrediente: 5, nome: 'Pecorino'},
+    {IDingrediente: 6, nome: 'Carne'},
+    {IDingrediente: 7, nome: 'Sale'},
+    {IDingrediente: 8, nome: 'Pepe'},
+    {IDingrediente: 9, nome: 'Zucchine'},
+    {IDingrediente: 10, nome: 'Mascarpone'},
+    {IDingrediente: 11, nome: 'Cacao'},
+    {IDingrediente: 12, nome: 'Caffe'},
+    {IDingrediente: 13, nome: 'Savoiardi'}];
 
   constructor(private http: HttpClient) { }
 
   // metodo per recuperare tutti gli ingredienti
   getAll(): Observable<Ingrediente[]> {
-    return this.http.get<Ingrediente[]>(URL_API.allIngredienti);
+    // return this.http.get<Ingrediente[]>(URL_API.allIngredienti);
+    return of(this.ingredienti);
   }
 
   // metodo per recuperare un ingrediente passando come parametro il codice identificativo
