b3f8e4f10cb07994f2cc6743232d108bd547c085
Salvatore <s.salernitano@gp-start.it>
2020-07-04 18:11:09 +0200

    Create Server

---
+++ UnicookServer/src/test/java/com/uivaq/mobile/unicookserver/UnicookserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uivaq.mobile.unicookserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnicookserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
